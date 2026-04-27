6352093271247a203ed029ad3c602b289f8a0821
Mostafa-Tech-Thabat <m.soliman@thabatholding.com>
2026-04-27 21:09:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,87 @@
+// Thabat Holding — Investor Portal
+// auth config — DO NOT commit this file to GitHub
+// Keep this file ONLY on your local machine and upload manually to your hosting
+
+window.__AUTH = {
+  entries: [
+    {
+      h: "a7fcf276d7e6f838551040e51868b12c4acdcbd06b02487879d43257535c56f66db0888ddc8348cf62a5adb3da57b79e25d547bbfcdf74436982bfcc932218a6",
+      s: "465bacc22f74923da05569d77ccd2f725bfda4791be8252e7bb3aaa60135264c",
+      n: "Abdullah Almunif",
+      r: "Chairman & Group CEO",
+      i: "AA",
+      p: "17.5",
+      t: "Chairman & Group CEO · Thabat Holding",
+      uk: "7d38852ae00d6bc23def586df1f3de8a22ec7d09cd5f32aea01cfe9c1140a2db"
+    },
+    {
+      h: "a4abe8c2f7054a971af4e23204b435208f901caaa0b6b04d785857bc92956556d384e25edba5bffcea228a9a8708664e85829e513f9141d2af1031b8a7cb9628",
+      s: "7aa26e6721b2dec8b7e24d66754334f7a7369b26d00fb4a9c3e56e18332ee665",
+      n: "Ali Almunif",
+      r: "Shareholder",
+      i: "AL",
+      p: "16.5",
+      t: "Shareholder · Thabat Holding",
+      uk: "94419b99b12c11133a4dfeccc3e17885974beb48f7827c48239aabfbcad238d8"
+    },
+    {
+      h: "eccd322cefc4e47e6232ad6902b8cb1c6677a65b88b6ccbfac0d472431c520100b3c981a5e750388c628f7f43447353daa1d1a83e06dbd12250e2321c7fe859d",
+      s: "36233eb6c1a9f430533885f4f25300e49b671f4dd58778d9fe8a677eef9ab3c8",
+      n: "Ibrahim Almunif",
+      r: "Shareholder",
+      i: "IA",
+      p: "16.5",
+      t: "Shareholder · Thabat Holding",
+      uk: "9711bcc773b67df9d02c1adc26240b1332048ab8ec17a23732ace9e0c030e0bd"
+    },
+    {
+      h: "bf1a32895b2c5a4f9dbffbf3861d0369dd2953b83cecc63daa1b685d0153c524e6f83afc119c9960f6ba582f166373d0bdfdb97cf15c1ec8da5c14cf34178fa8",
+      s: "61170be1485f7a1b2ff45e2852ae0698e349a5fc4fbb4ba06c84b653e3865fc4",
+      n: "Maha Almunif",
+      r: "Shareholder",
+      i: "MA",
+      p: "16.5",
+      t: "Shareholder · Thabat Holding",
+      uk: "d980e1b9ef2c97be6f16b2bbb18e71891a9257942412d69e101524c511642d96"
+    },
+    {
+      h: "3b90ad6ce61d029d41101a6d86849db61b5650d810ce7db063f60bc696dae682e0989b531ad8e00f72257a9251a16d89c77f0bd981cf75037ee66b0a431b5167",
+      s: "1260c588d317f26eaf47c5d47549bc95b956b6b0e4de5f08b00395592897752d",
+      n: "Mari Almunif",
+      r: "Shareholder",
+      i: "MR",
+      p: "16.5",
+      t: "Shareholder · Thabat Holding",
+      uk: "fbc835d2c63c85a2af298286581b9b77e8f1d5436a7ccbe391fceecce2a4729e"
+    },
+    {
+      h: "1bee0d63ba3235a977f1045f270fbdd6f43a13a932e226e412e38bb61253c3d88477b401d77f44f0e3225d1e7e4ef774232ca6ed7e3ca5f19de84f2a3cafc9f8",
+      s: "b1c48d8c59e633fe83f64184df965a0b74c3453a195f4dbb53442ddca7b667c7",
+      n: "Munif Almunif",
+      r: "Shareholder",
+      i: "MN",
+      p: "16.5",
+      t: "Shareholder · Thabat Holding",
+      uk: "4414be8ba7bd32dab555569e9d21acf5e6077112f4ba42c457774954f8e80344"
+    },
+    {
+      h: "69b9e98300b3705a67f99197589be596f9b3caaf4333a10ee91f6e2584a2ab3ceda5d28707f619b78dda3d2fd9a463bd04d36db9464955a70d405c8bcf5a5184",
+      s: "779719232105c2b5e91d9658e1841a62e8f9f0681b358f008010cab27bbf2b50",
+      n: "Portal Admin",
+      r: "Administrator",
+      i: "AD",
+      p: "—",
+      t: "Administrator · Thabat Holding",
+      uk: "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
+    }
+  ],
+  lookup: {
+    "7d38852ae00d6bc23def586df1f3de8a22ec7d09cd5f32aea01cfe9c1140a2db": 0,
+    "94419b99b12c11133a4dfeccc3e17885974beb48f7827c48239aabfbcad238d8": 1,
+    "9711bcc773b67df9d02c1adc26240b1332048ab8ec17a23732ace9e0c030e0bd": 2,
+    "d980e1b9ef2c97be6f16b2bbb18e71891a9257942412d69e101524c511642d96": 3,
+    "fbc835d2c63c85a2af298286581b9b77e8f1d5436a7ccbe391fceecce2a4729e": 4,
+    "4414be8ba7bd32dab555569e9d21acf5e6077112f4ba42c457774954f8e80344": 5,
+    "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918": 6
+  }
+};
